f71614b7327cda6d951f47575485abcefeb5e6d1
Tolu Shobande <tolu.shobande@gl05811m.gamesys.corp>
2016-11-30 14:45:03 +0000

    added routing

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ var app = express()
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+//refresh browser get request
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
+app.get('/user', function (req, res) {
+  res.send('Got a GET request at /user')
+})
 
 app.listen(3000, function () {
   console.log('Example app 3000')
