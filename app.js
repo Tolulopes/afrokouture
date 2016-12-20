e61d9a17fcb5234c056bcf2aeb871d959f3fc587
Tolu Shobande <tolu.shobande@gl05811m.gamesys.corp>
2016-12-20 15:57:27 +0000

    adding data

--- app.js
+++ app.js
@@ -2,21 +2,60 @@ var express = require('express')
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Afrokouture = require('./models/afrokouture');
 
 mongoose.connect('mongodb://localhost/afrokouture');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  //we are connected
+  console.log('we are connected');
 });
 
 app.get('/', function(req, res){
   res.send('please use api/afrokouture/ ');
 });
 
-app.get('afrokouture/', function(req, res){
-  res.send('you are now on the afrokouture home page');
+app.get('/api/stylists/name/:name', function(req, res){
+  console.log(req.params);
+  Afrokouture.findOne({ name: req.params.name }, function(error, output){
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    if (output === null) {
+      return res.sendStatus(404);
+    }
+
+    res.json(output);
+  });
+})
+
+app.post('/api/stylists', function(req, res){
+  var stylist = new Afrokouture(req.body);
+
+  stylist.save(function(err, doc) {
+    if (err) {
+      res.error(err);
+    } else {
+      console.log('doc', doc);
+      res.send(doc);
+    }
+  })
+})
+
+app.put('/api/stylists', function(req, res){
+  var stylist = new Afrokouture(req.body);
+
+  stylist.save(function(err) {
+    if (err) {
+      res.error(err);
+    } else {
+      res.send();
+    }
+
+  })
 })
 
 app.listen(3000, function () {
--- models/afrokouture.js
+++ models/afrokouture.js
@@ -1,17 +1,19 @@
-  var mongoose = require('mongoose');
-  var Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-  var AfrokoutureSchema = new Schema({
-    name: String,
-    profession: String,
-    specialism: String,
-    description: String,
-    contactDetails: String,
-    socialMedia: String,
-    location: String,
-    website: String,
-  });
+var AfrokoutureSchema = new Schema({
+  name: String,
+  profession: String,
+  specialism: String,
+  description: String,
+  contactDetails: String,
+  socialMedia: String,
+  location: String,
+  website: String,
+});
 
 
-  var Afrokouture = mongoose.model('afrokouture', afrokoutureSchema);
-  //ready to go
+var Afrokouture = mongoose.model('afrokouture', AfrokoutureSchema);
+//ready to go
+
+module.exports = Afrokouture;
